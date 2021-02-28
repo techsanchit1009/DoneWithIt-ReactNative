492c6d49a5721e670d0f0f2403e594152c757f64
Sanchit Sachdeva <sanchit.sachdeva@tothenew.com>
2021-02-28 19:47:31 +0530

    updated settings file

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,7 +2,7 @@ import Constants from 'expo-constants';
 
 const settings = {
   dev: {
-    apiUrl: 'http://192.168.0.115:9000/api'
+    apiUrl: 'https://donewithit-node-deploy.herokuapp.com/api'
   },
   staging: {
     apiUrl: 'https://donewithit-node-deploy.herokuapp.com/api'
